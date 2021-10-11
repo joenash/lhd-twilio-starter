1e536c93cefd2b7fa72f423f974d8e78105f5558
joenash <1790822+joenash@users.noreply.github.com>
2021-10-11 20:27:01 +0200

    post workshop

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
+const twilio = require("twilio");
 const express = require("express");
-
+const MessagingResponse = require("twilio/lib/twiml/MessagingResponse");
 const app = express();
 const port = 3000;
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH;
+const twilioPhone = process.env.TWILIO_NUMBER;
+
+const client = new twilio(accountSid, authToken);
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/text", (req, res) => {
+  client.messages
+    .create({
+      body: "Hello Local Hack Day",
+      to: process.env.JOE_PHONE,
+      from: twilioPhone,
+    })
+    .then((message) => {
+      console.log(`Message sent: ${message.sid}`);
+      res.send(`Message sent: ${message.sid}`);
+    });
+});
+
+app.post("/incoming", (req, res) => {
+  console.log(req.body.Body);
+
+  const twiml = new MessagingResponse();
+
+  twiml.message(`You are in Guild: ${req.body.Body}`);
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
 });
 
 app.listen(port, () => {
